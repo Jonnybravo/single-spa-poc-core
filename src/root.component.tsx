58564c85df71ee348c20b6c3fcaf85a4adee70f4
Taborda, Joao <Joao.Taborda@pmi.com>
2021-02-11 15:31:00 +0000

    - phase 1: core w/ navbar + app

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+  console.log("HEY");
+  return (
+    <nav>
+      <a href="/">Home</a> |<a href="/app1">App 1</a> |<a href="/app2">App 2</a>
+    </nav>
+  );
 }
